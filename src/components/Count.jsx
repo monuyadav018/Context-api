750fe05f92f711cfa77b620563de924427cddacc
Monu Yadav <104672663+monuyadav018@users.noreply.github.com>
2025-12-01 17:50:47 +0530

    Update Count.jsx
    How to use the "context using useContext"

--- src/components/Count.jsx
+++ src/components/Count.jsx
@@ -4,11 +4,14 @@ import { ConterContext } from "../context/Counter";
 
 
 const Count =()=>{
-    const counterState = useContext(ConterContext);
+    // const counterState = useContext(ConterContext);
+    const {increase,decrease} = useContext(ConterContext);
     return(
         <div >
-            <button onClick={counterState.setCount(ConterContext.count+1)}>Increment</button>
-            <button onClick={counterState.setCount(ConterContext.count-1)}>decrement</button>
+            {/* <button onClick={counterState.setCount(ConterContext.count+1)}>Increment</button> */}
+            {/* <button onClick={counterState.setCount(ConterContext.count-1)}>decrement</button> */}
+            <button onClick={increase}>inc</button>
+            <button onClick={decrease}>dec</button>
         </div>
     );
 };
