a2e52bac97a63957b973db311a74aefe2061c9d1
Monu Yadav <104672663+monuyadav018@users.noreply.github.com>
2025-12-01 17:54:40 +0530

    Update Counter.jsx
    update "CounterProvider"

--- src/context/Counter.jsx
+++ src/context/Counter.jsx
@@ -5,8 +5,13 @@ export const ConterContext = createContext(null);
 // now make the provider
 export const CounterProvider = (props) => {
     const [count, setCount] =useState(5);
+    const increase =()=>{
+        setcount(count+1)}
+    const decrease =()=>{
+        setCount(count-1);}
   return (
-    <ConterContext.Provider value={{count,setCount, name:"monu"}} >
+    // <ConterContext.Provider value={{count,setCount, name:"monu"}} >
+      <ConterContext.Provider value={{count,setCount,increase,decrease}}>
       {props.children}
     </ConterContext.Provider>
   );
